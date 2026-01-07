686d4bdd88ab0d3231e274ee83e8535f5313cc43
lidin888 <lidin888@github.com>
2026-01-07 16:44:23 +0800

    fix: 添加 GET_BYTE 宏定义以解决编译错误

--- board/can_declarations.h
+++ board/can_declarations.h
@@ -27,3 +27,4 @@ typedef struct {
 #define GET_BUS(msg) ((msg)->bus)
 #define GET_LEN(msg) (dlc_to_len[(msg)->data_len_code])
 #define GET_ADDR(msg) ((msg)->addr)
+#define GET_BYTE(msg, idx) ((msg)->data[idx])
